36c555208f7734368bf38c0d176cbe3316c4fe87
Ahmed Ibrahim <midoibrahim432@gmail.com>
2024-12-28 05:31:27 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from helper import preprocessing_step
+import sklearn
+import pickle
+import nltk
+
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+# loading models
+model = pickle.load(open('models/model.pkl','rb'))
+vectorizer = pickle.load(open('models/vectorizer.pkl','rb'))
+
+# title of the program
+st.title('Sentiment Analysis using Machine learning')
+
+# user review
+review = st.text_input('Please enter your review')
+
+state = st.button('predict')
+#1 preprocessing step
+tokens = preprocessing_step(review) 
+
+#2 vectorizer transform step
+
+transformed_data = vectorizer.transform([tokens])
+
+# predict output
+
+output = model.predict(transformed_data)
+
+
+# predict button
+
+if state:
+    st.text(f'output = {'Good Review' if output ==1 else 'Bad Review'}')
+else:
+    st.text('stable')
